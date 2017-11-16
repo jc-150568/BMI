50c2f466d0cc1dea562186695520614b0a5280a8
遼太郎 金 <150568@jc-21.jp>
2017-11-16 14:53:56 +0900

    プロジェクト ファイルを追加します。

---
+++ BMI/BMI/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BMI
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Button_Clicked(object sender,EventArgs e)
+        {
+           /* Double w = Double.Parse(Weight.Text);
+            Double h = Double.Parse(Height.Text);
+
+            if (h >= 3)
+            {
+                h /= 100;
+            }
+            double bmi = w / h / h;
+            DisplayAlert("BMI", bmi.ToString, "OK");*/
+        }
+	}
+}
